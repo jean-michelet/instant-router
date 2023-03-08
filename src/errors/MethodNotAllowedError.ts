0cacf599fadcc3594281673c4ec742911a086c9c
Jean <jean.antoine.michelet@gmail.com>
2023-03-08 08:09:57 +0100

    set MethodNotAllowedError default message mor generic

--- src/errors/MethodNotAllowedError.ts
+++ src/errors/MethodNotAllowedError.ts
@@ -1,12 +1,7 @@
-import RequestContext from '../RequestContext'
-
 export default class MethodNotAllowedError extends Error {
-  constructor (method: string) {
-    const message = `Invalid HTTP method '${method}'. The '${method}' method is not allowed by the RequestContext class.\n` +
-                    `Use a valid HTTP method: ${RequestContext.availableHttpMethods.join(', ')}.\n` +
-                    'To add a custom HTTP method, update the \'availableHttpMethods\' static array in the RequestContext class.'
-
+  constructor (method: string, message: string = `Method "${method}" is not allowed.`) {
     super(message)
+
     this.name = 'MethodNotAllowedError'
   }
 }
--- src/tests/errors/MethodNotAllowedErrorTest.ts
+++ src/tests/errors/MethodNotAllowedErrorTest.ts
@@ -1,15 +1,11 @@
-import RequestContext from '../../RequestContext'
 import MethodNotAllowedError from '../../errors/MethodNotAllowedError'
 
 describe('MethodNotAllowedError', () => {
   test('should have correct message', () => {
     const method = 'PATCH'
     const error = new MethodNotAllowedError(method)
-    const expectedMessage = `Invalid HTTP method '${method}'. The '${method}' method is not allowed by the RequestContext class.\n` +
-                             `Use a valid HTTP method: ${RequestContext.availableHttpMethods.join(', ')}.\n` +
-                             'To add a custom HTTP method, update the \'availableHttpMethods\' static array in the RequestContext class.'
 
-    expect(error.message).toBe(expectedMessage)
+    expect(error.message).toBe('Method "PATCH" is not allowed.')
   })
 
   test('should have correct name', () => {
